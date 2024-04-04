92bf0fdaaf0b9d6b132df825f2088f720a8d6bec
ChunPanYung <31672021+ChunPanYung@users.noreply.github.com>
2024-04-04 16:43:46 -0400

    chore(pr): release `v.1.1.0`. (#6)
    * fix: do not put #! at the beginning, it may cause error.
    
    * doc: changelog to keep track of changes.
    
    * refactor: save dictionary value to variable.
    
    * feat: filter `desired_version` with regexp.
    
    * feat: documentation and additional allow user to specify command arg.
    
    * test: test case for new parameter for `cmp_pkg` module.
    
    * fix: search function instead of match.
    
    * fix: test different command.
    
    * doc: update documentation for release v1.1.0.

--- plugins/modules/cmp_pkg.py
+++ plugins/modules/cmp_pkg.py
@@ -32,6 +32,12 @@ options:
         aliases: [ desired_version ]
         required: true
         type: str
+    arg:
+        description:
+            - argument that will be appended to command, default is '--version'.
+            - example: if the command is 'bash', it will be 'bash --version'.
+        default: '--version'
+        type: str
     index:
         description:
             - Which version number selected from after running command.
@@ -59,6 +65,7 @@ EXAMPLES = r"""
   ansi_colle.mods.cmp_pkg:
     name: ansible
     index: 1
+    arg: '--version'
     desired_version: 3.12.1
 
 # fail the module
@@ -104,6 +111,7 @@ def run_module():
         name=dict(type="str", required=True, aliases=["command_name"]),
         version=dict(type="str", required=True, aliases=["desired_version"]),
         regexp=dict(type="str", default=r"\d+\.\d+\.\d+"),
+        arg=dict(type="str", default=r"--version"),
         index=dict(type="int", default=0),
     )
 
@@ -127,15 +135,16 @@ def run_module():
         module.exit_json(**result)
 
     # Append '--version' to args and get command version
-    args.append("--version")
+    args.append(module.params["arg"])
     rc, stdout, stderr = module.run_command(args)
 
     # Return list of version after re.findall() function
-    result["version_list"] = re.findall(module.params["regexp"], stdout)
+    regexp: str = module.params["regexp"]
+    result["version_list"] = re.findall(regexp, stdout)
     # Get only selected version
     index: int = module.params["index"]
     installed_version = result["version_list"][index]
-    desired_version = module.params["version"]
+    desired_version = re.search(regexp, module.params["version"]).group(0)
 
     if desired_version < LooseVersion(installed_version):
         result["msg"] = (
